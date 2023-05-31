5d29482457535f27bcdbc39e3fc43912ef75b21a
Manolis Andreopoulos <manolisandreopoulos@gmail.com>
2023-05-31 18:44:36 +0300

    Read API KEYS

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 enable_VT = False
 enable_AbuseIP = False
 IP_Addresses = list()
+VT_API_KEYS = list()
+AIP_API_KEYS = list()
 input_filename = "input_list.txt"
 output_filename = "output_list.txt"
 VT_API_KEYS_filename = "VT_API_KEYS.txt"
@@ -14,7 +16,7 @@ AIP_API_KEYS_filename = "AIP_API_KEYS.txt"
 
 def init_configuration():
     try:
-        with open("configurations.json", "r") as jsonfile:
+        with open("configuration.json", "r") as jsonfile:
             configuration = json.load(jsonfile)
     except FileNotFoundError:
         print("Configuration file not found!\n\nExample of json configuration file:\n")
@@ -53,6 +55,19 @@ def init_configuration():
         AIP_API_KEYS_filename = configuration["AIP_API_KEYS_filename"]
 
 
+def init_api_keys():
+    # VirusTotal API KEYS
+    with open(VT_API_KEYS_filename, "r") as vt_file:
+        for line in vt_file:
+            global VT_API_KEYS
+            VT_API_KEYS.append(line.replace('\n', ''))
+    # AbuseIPDB API KEYS
+    with open(AIP_API_KEYS_filename, "r") as AIP_file:
+        for line in AIP_file:
+            global AIP_API_KEYS
+            AIP_API_KEYS.append(line.replace('\n', ''))
+
+
 def read_input_file():
     # Regular expression for IP addresses
     IP_regex = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
@@ -60,13 +75,17 @@ def read_input_file():
         for line in input_file:
             match = re.search(IP_regex, line)
             if match:
+                global IP_Addresses
                 IP_Addresses.append(match.group())
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Read configuration file
     init_configuration()
+    # Read API KEYS from file
+    init_api_keys()
+    # Read input file and add IP addresses to list
     read_input_file()
-    print(enable_VT)
-    print(enable_AbuseIP)
-    print(input_filename)
+    print(IP_Addresses)
+    print(VT_API_KEYS)
+    print(AIP_API_KEYS)
